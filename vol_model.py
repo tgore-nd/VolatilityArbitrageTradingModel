00e9e56b17fcf2fdfe04019fb4ae83061df140d1
Tom Gore <tgore@nd.edu>
2025-08-30 21:26:29 -0400

    Working version

--- vol_model.py
+++ vol_model.py
@@ -1,13 +1,11 @@
 import numpy as np
-import polars as pl
-import duckdb
 import torch
 import torch.nn as nn
 import time
 import xgboost as xgb
 import matplotlib.pyplot as plt
-from torch.utils.data import Dataset, DataLoader, TensorDataset
-from datetime import timedelta
+from torch.utils.data import DataLoader, TensorDataset
+from data.data_acquisition import get_data
 from sklearn.metrics import root_mean_squared_error
 from typing import Literal
 from pathlib import Path
@@ -61,11 +59,12 @@ class NextStepHead(nn.Module):
 
 # SSL Training Loop
 def train_self_supervised(encoder: LSTMEncoder, head: NextStepHead, train_loader: DataLoader, epochs: int = 5, lr: float = 1e-3, device: Literal["cpu", "cuda"] = "cpu", saved_model_path: str = "encoder.torch") -> tuple[LSTMEncoder, nn.Sequential]:
+    model = nn.Sequential(encoder, head).to(device)
     if Path(saved_model_path).is_file():
         encoder.load_state_dict(torch.load(saved_model_path, weights_only=True))
-        return encoder, nn.Sequential()
+        model.load_state_dict(torch.load("model.torch", weights_only=True))
+        return encoder, model
 
-    model = nn.Sequential(encoder, head).to(device)
     opt = torch.optim.Adam(model.parameters(), lr=lr)
     loss_fn = nn.MSELoss()
 
@@ -94,7 +93,7 @@ def train_self_supervised(encoder: LSTMEncoder, head: NextStepHead, train_loader
     return encoder, model  # trained encoder, head discarded later
 
 
-def extract_features(encoder: LSTMEncoder, X: np.ndarray, batch_size: int = 128, device: Literal["cpu", "cuda"] = "cpu") -> np.ndarray:
+def extract_features(encoder: LSTMEncoder, X: np.ndarray, batch_size: int = 4096, device: Literal["cpu", "cuda"] = "cpu") -> np.ndarray:
     encoder.eval()
     feats = []
 
@@ -103,58 +102,53 @@ def extract_features(encoder: LSTMEncoder, X: np.ndarray, batch_size: int = 128,
         i = 1
         for xb in loader:
             print(f"Count: {i}")
-            h = encoder(xb[0])
+            if isinstance(xb, (list, tuple)):
+                xb = xb[0]
+            xb: torch.Tensor = xb.to(device)
+            h = encoder(xb)
             feats.append(h.cpu())
             i += 1
     return torch.cat(feats).numpy()  # (n_samples, hidden_dim)
 
 
-def get_data(symbol: str, equity_delta_path: str, options_delta_path: str, contract_id: str | None = None, filter_data_by_contract_duration: bool = True) -> pl.DataFrame:
-    options_data = duckdb.sql(f"SELECT * FROM delta_scan('{options_delta_path}') WHERE symbol = '{symbol}' ORDER BY date").pl()
-    equity_data = duckdb.sql(f"SELECT close, STRPTIME(timestamp, '%Y-%m-%d %H:%M:%S') AS date_col, date_trunc('day', date_col) AS date FROM delta_scan('{equity_delta_path}') WHERE ticker = '{symbol}' AND HOUR(date_col) = 16 AND MINUTE(date_col) = 0 ORDER BY date_col").pl().with_columns((pl.col("close") / pl.col("close").shift(1) - 1).alias("returns")).drop_nulls()
-    data_full = options_data.join(equity_data["date", "close", "returns"], on="date", how="inner").with_columns(
-        (pl.col("returns").rolling_std_by(pl.col("date"), timedelta(days=5)) * np.sqrt(252)).alias("realized_volatility_5"),
-        (pl.col("returns").rolling_std_by(pl.col("date"), timedelta(days=21)) * np.sqrt(252)).alias("realized_volatility_21"),
-        (pl.col("returns").rolling_std_by(pl.col("date"), timedelta(days=60)) * np.sqrt(252)).alias("realized_volatility_60"),
-        (pl.col("returns").pow(2)).alias("squared_returns")
-    )
+def model_predictions(model: nn.Sequential, X: np.ndarray, batch_size: int = 4096, device: Literal["cpu", "cuda"] = "cpu") -> np.ndarray:
+    """Produce predictions for the non-XGBoost model (for comparison purposes)."""
+    model.eval()
+    preds = []
 
-    if contract_id is None:
-        chosen_contract: str = data_full["contractID"].sample(1).item()
-    else:
-        chosen_contract = contract_id
-    
-    if filter_data_by_contract_duration:
-        data_full = data_full.filter(pl.col("contractID") == chosen_contract)
-        return data_full["returns", "implied_volatility", "realized_volatility_5", "realized_volatility_21", "realized_volatility_60"]
-
-    return data_full["returns", "realized_volatility_5", "realized_volatility_21", "realized_volatility_60"]
+    loader = DataLoader(TensorDataset(torch.tensor(X.astype(np.float32))), batch_size=batch_size)
+    with torch.no_grad():
+        i = 1
+        for xb in loader:
+            print(f"Count: {i}")
+            if isinstance(xb, (list, tuple)):
+                xb = xb[0]
+            xb: torch.Tensor = xb.to(device)
+            h = model(xb)
+            preds.append(h.cpu())
+            i += 1
+    return torch.cat(preds).numpy()
 
 
-if __name__ == "__main__":
-    start = time.perf_counter()
-
+def produce_predictions(save_models: bool = False, train_fraction: float = 0.8) -> tuple[np.ndarray, np.ndarray]:
+    """Produce predictions for the LSTM + XGBoost model."""
     # Acquire data
-    symbol = "AAPL"
-    equity_delta_path = r"C:\Users\tfgor\Documents\BayesianMarketMaker\data\deltalake" # remove explicit paths before posting this to GitHub
-    options_delta_path = r"C:\Users\tfgor\Documents\OptionsVolatiltyTrader\data\deltalake"
-    data = get_data(symbol, equity_delta_path, options_delta_path, filter_data_by_contract_duration=False)
+    data = get_data("AAPL", filter_data_by_contract_duration=False)
     X_raw = data[:-1]
     y_raw = data["realized_volatility_21"].shift(-1).drop_nulls() # next day volatility
 
-    # Build self-supervised windows to predict next-step return (no labels needed)
+    # Build self-supervised windows to predict next-step return
     T = 64
     X, y = create_sequences(X_raw.to_numpy(), y_raw.to_numpy(), T)  # self-supervised pairs (predict y_{t+T} from window ending at t + T - 1)
 
-
-    # Chronological split
+    # Dataset construction
     n = len(X)
-    split = int(n * 0.8)
+    split = int(n * train_fraction)
     X_train, X_test = X[:split], X[split:]
     y_train, y_test = y[:split], y[split:]
 
     dataset_train = TensorDataset(torch.tensor(X_train), torch.tensor(y_train))
-    dataset_test = TensorDataset(torch.tensor(X_test), torch.tensor(y_test))
+    # dataset_test = TensorDataset(torch.tensor(X_test), torch.tensor(y_test))
     dataloader_train = DataLoader(dataset_train, batch_size=128, shuffle=False)
 
     # SSL Training
@@ -162,10 +156,12 @@ if __name__ == "__main__":
     hidden_dim = 64
     encoder = LSTMEncoder(input_dim=X.shape[2], hidden_dim=hidden_dim, num_layers=1)
     head = NextStepHead(hidden_dim)
-    trained_encoder, model = train_self_supervised(encoder, head, dataloader_train, epochs=10, lr=1e-3, device=device)
-    torch.save(trained_encoder.state_dict(), "encoder.torch")
-    torch.save(model.state_dict(), "model.torch")
+    trained_encoder, model = train_self_supervised(encoder, head, dataloader_train, epochs=5, lr=1e-3, device=device)
 
+    if save_models:
+        torch.save(trained_encoder.state_dict(), "encoder.torch")
+        torch.save(model.state_dict(), "model.torch")
+    
     # Extract embeddings
     train_embed = extract_features(trained_encoder, X_train, device=device)  # (split, emb_dim)
     test_embed = extract_features(trained_encoder, X_test, device=device)
@@ -182,14 +178,24 @@ if __name__ == "__main__":
         "colsample_bytree": 0.8,
         "tree_method": "hist"  # fast
     }
-
     booster = xgb.train(params, dtrain, num_boost_round=200)
-
     y_preds = booster.predict(dtest)
     rmse = root_mean_squared_error(y_test, y_preds)
+
     print(f"[Downstream XGB] Vol target RMSE: {rmse:.6f}")
     print(f"Time taken: {time.perf_counter() - start} seconds")
 
+    return y_test, y_preds
+
+
+if __name__ == "__main__":
+    start = time.perf_counter()
+
+    y_test, y_preds = produce_predictions(save_models=False)
+
     plt.plot(y_test, label="Actual")
     plt.plot(y_preds, label="Predicted")
+    plt.legend()
     plt.show()
+
+    print("Done")
